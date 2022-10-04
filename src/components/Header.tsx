eb675e4107049edf5e70d53f67b5a7c3508d0779
JairoDosSantos <jairocrissantos@gmail.com>
2022-10-04 15:34:39 +0100

    Correção do desaparecimento do email no header quando a página é actualizada

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,37 @@
-
+import { useEffect, useState } from 'react'
 import Image from "next/image"
+
 
 
 import User from '../assets/user.png'
 import { FaBars } from 'react-icons/fa'
 import { useSelector } from "react-redux";
 import { useDispatch } from 'react-redux';
-import { hideSiderBar } from '../redux/slices/geralSlice';
-
+import { hideSiderBar, updateUser } from '../redux/slices/geralSlice';
+import cookie from 'nookies'
+import { useRouter } from 'next/router';
 
 const Header = () => {
 
     const dispatch = useDispatch()
-
+    const route = useRouter()
     const { user, showSideBar } = useSelector((state: any) => state.geral)
-
+    const [userEmail, setUserEmail] = useState(user)
     const changeSideBarVisibility = () => {
         dispatch(hideSiderBar({ showSideBar: !showSideBar }))
     }
+
+    useEffect(() => {
+
+        const UserEmail = cookie.get(null)
+        if (UserEmail.USER_LOGGED_ARMAZEM) {
+
+            const emailUserParse = JSON.parse(UserEmail.USER_LOGGED_ARMAZEM)
+
+            dispatch(updateUser(emailUserParse.email))
+            setUserEmail(emailUserParse.email)
+        }
+    }, [route.pathname])
 
     return (
         <header className=" bg-white p-5 flex justify-between shadow z-50">
@@ -27,7 +41,7 @@ const Header = () => {
                 </span>
             </div>
             <div className="flex items-center gap-4">
-                <span className="text-gray-400 font-bold">{user}</span>
+                <span className="text-gray-400 font-bold">{userEmail}</span>
                 <Image src={User} className='rounded-full' width={25} height={25} objectFit={"contain"} />
             </div>
         </header>
