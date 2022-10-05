33c0dc42e6b0187f843de3af64382bc828e27c97
JairoDosSantos <jairocrissantos@gmail.com>
2022-10-05 16:49:12 +0100

    Implementado a PWA no projecto

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 module.exports = {
   reactStrictMode: true,
     images: {
     domains: ['i.ibb.co'],
   },
-}
+},
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
+
