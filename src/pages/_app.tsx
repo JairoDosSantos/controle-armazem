1ddd336d2b80a7ca82c1b91182f27e54c0c56b01
JairoDosSantos <jairocrissantos@gmail.com>
2022-10-05 16:53:13 +0100

    Adicionado o meta viewPort

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Sistema de Controle de Armazem</title>
         {/**  <link rel="icon" href='/noah.png' /> */}
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+        />
       </Head>
 
       <Component {...pageProps} />
